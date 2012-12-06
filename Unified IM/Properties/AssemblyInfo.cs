315227c0a7ad9a0b718593ebf39f98e93d202477
Gideon Sireling <code@accursoft.com>
2012-12-06 11:08:59 +0200

    assembly metadata

--- Unified IM/Properties/AssemblyInfo.cs
+++ Unified IM/Properties/AssemblyInfo.cs
@@ -5,9 +5,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Unified IM")]
 [assembly: AssemblyCompany("Accursoft")]
 [assembly: AssemblyProduct("Unified IM")]
+[assembly: AssemblyDescription("Unified .NET API for MSN, Yahoo, ICQ, AIM, and Jabber/GTalk")]
+[assembly: AssemblyCopyright("GPLv3")]
 
 [assembly: Guid("17778ea1-e617-4414-a8ce-9b12523dd88d")]
-[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
 
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
